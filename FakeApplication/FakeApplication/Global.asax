f8073b0a87b751467885aa489b93c1ae4f402379
Алекс Инджов <sashe94@hotmail.com>
2017-04-21 13:51:23 +0300

    Commit Fake Project

---
+++ FakeApplication/FakeApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FakeApplication.MvcApplication" Language="C#" %>
---
+++ FakeApplication/FakeApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FakeApplication.Startup))]
+namespace FakeApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
